a4c5572578614bee0941741eb3f5b51f4ce33f84
azrosen92 <azrosen92@gmail.com>
2017-02-06 17:49:05 -0500

    stuff

--- lib/acme_udp_logger.ex
+++ lib/acme_udp_logger.ex
@@ -1,2 +1,14 @@
 defmodule AcmeUdpLogger do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = []
+
+    # Starts the main supervision tree, and restart failed children individually.
+    opts = [strategy: :one_for_one, name: AcmeUdpLogger.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
 end
